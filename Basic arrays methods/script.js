06162357281b6151343028112e4907ef5a048705
QaqasAzizov <qaqasazizov35@gmail.com>
2023-07-07 07:46:57 -0800

    array methods

---
+++ Basic arrays methods/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// const myPop = (array) => {
+//   let removed = array[array.length - 1];
+//   array.length = array.length - 1;
+//   return removed;
+// };
+
+// console.log(myPop(["Toyota", "Honda", "Ford", "Chevrolet", "BMW"]));
+
+// const myPush = (array, subject) => {
+//     array[array.length] = subject;
+
+//     return array.length;
+// }
+
+// console.log(myPush(["Toyota", "Honda", "Ford", "Chevrolet", "BMW"] , "Lada"));
+
+//
+
+// const myShift = (array) => {
+//   let removed = array[0];
+//   for (let i = 0; i < array.length; i++) {
+//     array[i] = array[i + 1];
+//   }
+//   array.length = array.length - 1;
+//   return removed;
+// };
+
+// console.log(myShift(["Toyota", "Honda", "Ford", "Chevrolet", "BMW"]));
+
+//
+
+// const myUnshift = (array , value) => {
+//     for (let i = array.length; i >= 0; i--) {
+//         array[i] = array[i-1];
+        
+//     }
+//     array[0] = value;
+//     return array;
+// }
+
+// console.log(myUnshift(["Toyota", "Honda", "Ford", "Chevrolet", "BMW"] , "Lada"));
